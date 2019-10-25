37a871082d5d5871e4a51a2ad99ae88470d2d275
SoengSaravit <soengsaravit@gmail.com>
2019-10-25 13:45:05 +0700

    Start Project

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Soeng Saravit on 10/25/17.
+//  Copyright © 2017 Soeng Saravit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+}
+
+@IBDesignable class customButton: UIButton {
+    
+    @IBInspectable
+    public var cornerRadius: CGFloat = 0.0 {
+        didSet {
+           self.layer.cornerRadius = self.cornerRadius
+        }
+    }
+}
